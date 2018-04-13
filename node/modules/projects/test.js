745fc14cf4a5bc50dff0dbc4d1f5664aaeac69e9
Dennis <dennis.spierenburg@hotmail.com>
2018-04-13 10:57:31 +0200

    static home page

---
+++ node/modules/projects/test.js
@@ -0,0 +1,11 @@
+const yourName = {
+  name: "yourName",
+  url: "http://yourwebsite.com",
+  principle: [1, 2, 4],
+  gif: false,
+  longImage: true,
+  image: '../images/yourimage.png',
+  description: ''
+};
+
+module.exports = yourName;
