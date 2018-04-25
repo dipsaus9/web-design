dcec35108d9acdc6e2f532f3fa0e05321d35e4a0
Dennis <dennis.spierenburg@hotmail.com>
2018-04-25 13:44:00 +0200

    update for larissa

--- node/modules/projects/boz.js
+++ node/modules/projects/boz.js
@@ -4,8 +4,12 @@ const Stadsmakelaar = {
   case: "Makelaars website Bergen op Zoom",
   opdrachtgever: "Bergen op Zoom (Matise)",
   rol: "Webdevelopment",
-  beschrijving: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  korteBeschrijving: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea...",
+  beschrijving: `Stadsmakelaar Bergen op Zoom is een project waaraan ik werkzaam ben geweest bij het bedrijf Matise. Het doel van dit project was om een nieuw, modern en frisse website op te zetten voor het aanbod van huizen in de gemeente Bergen op Zoom. Op deze website kunnen gebruikers de huizenmarkt zien op de kaart en in een overzicht. Hier kunnen ze vervolgens een huis filteren en selecteren voor meer informatie.
+
+Voor een nieuwbouw project was het ook mogelijk om een calculatie te maken van het huis. Hier kon de gebruiker zijn / haar voorkeur invoeren en zo de calculatie tool gebruiken om zo tot een inzicht te komen in de kosten van het huis.
+
+Dit project bevat veel elementen. Dit project is opgebouwd in een custom theme in WordPress. Alle element zijn zelf geschreven en opgebouwd in modules. Ook zit er een groot mapbox element in waar alle huizen op te zien zijn.`,
+  korteBeschrijving: "Stadsmakelaar Bergen op Zoom is een project waaraan ik werkzaam ben geweest bij het bedrijf Matise. Het doel van dit project was om een nieuw, modern en frisse website op te zetten voor het aanbod van huizen in de gemeente Bergen op Zoom. Op deze website kunnen gebruikers de huizenmarkt zien op de kaart en in een overzicht. Hier kunnen ze vervolgens een huis filteren en selecteren voor meer informatie...",
   fullImage: '../images/bozFull-smaller.png',
   alt: 'Dit figuur is een representatie van de home page van Stadsmakelaar Bergen op Zoom. De home page van deze website bevat een map met alle locaties van de huizen die op dit moment te koop of te huur zijn in de gemeente Bergen op Zoom. Ook wordt hier beschreven hoe duur deze huizen zijn waarna je vervolgens verder kan kijken op een overzicht pagina.',
   code: ['HTML', 'CSS', 'JavaScript', 'WordPress'],
--- node/modules/projects/routeplanner.js
+++ node/modules/projects/routeplanner.js
@@ -4,8 +4,11 @@ const Stadsmakelaar = {
   case: "Inzicht geven in data van gemeente Amsterdam",
   opdrachtgever: "HvA en OBA",
   rol: "Webdevelopment en Design",
-  beschrijving: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  korteBeschrijving: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea...",
+  beschrijving: `De routeplanner voor de metro in Amsterdam is begonnen voor een project bij de OBA tijdens mijn minor Webdevelopment bij de HvA. Bij dit project kregen we data van de gemeente Amsterdam ter beschikking om zo een eigen concept te verzinnen. Mijn concept was dan ook om een routeplanner te maken met de nieuwe metrolijn in Amsterdam.
+
+Dit project begon als een onepage gebaseerd op een Javascript based framework. Dit project heb ik in een week gemaakt. Na deze week ben ik verder gegaan op dit project om hier een Node versie van te maken. Hierdoor werkt de appicatie ook zonder Javascript.
+`,
+  korteBeschrijving: "De routeplanner voor de metro in Amsterdam is begonnen voor een project bij de OBA tijdens mijn minor Webdevelopment bij de HvA. Bij dit project kregen we data van de gemeente Amsterdam ter beschikking om zo een eigen concept te verzinnen. Mijn concept was dan ook om een routeplanner te maken met de nieuwe metrolijn in Amsterdam....",
   fullImage: '../images/routeplanner2.gif',
   gif: true,
   alt: 'Dit is een bewegende afbeelding van de functionaliteit van deze website. Het laat zien hoe je een route kan plannen van deur tot deur in Amsterdam',
